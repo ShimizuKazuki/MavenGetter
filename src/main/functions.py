b38a779c45197f8849fac370653b14af10fad5fc
Shimizu Kazuki <shimizu@poslnoMacBook-Pro-2.local>
2020-11-27 14:15:42 +0900

    Get #Star #3 Fixed

--- src/main/functions.py
+++ src/main/functions.py
@@ -1,7 +1,23 @@
 import git
+import re
+import subprocess
 
 #Count Commit
 def get_commit_count(repo):
     commits = repo.get_commits()
-    return commits.totalCount
+    return str(commits.totalCount)
+
+def get_loc(reponame):
+    a = subprocess.run(["sh", "./other/git-loc.sh", reponame], encoding='utf-8', stdout=subprocess.PIPE)
+    match = re.search("SUM:\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)", a.stdout)
+    if match is None:
+        match = re.search(".+\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)", a.stdout)
+    if match:
+        return str(match.group(4))
+    else:
+        return str(0)
+
+def get_star_count(repo):
+    star = repo.get_stargazers()
+    return str(star.totalCount)
 
--- src/main/project.py
+++ src/main/project.py
@@ -22,24 +22,17 @@ def main():
         for reponame in reponames:
             out = []
             cloner.cloneRepo(reponame)
-            print(cloner.repoDir + reponame + "/pom.xml")
-            if not os.path.exists(cloner.repoDir + reponame + "/pom.xml"):
-                logging.info(reponame + " is not Maven Project")
-                shutil.rmtree(cloner.repoDir + reponame.split('/')[0])
+            # if not os.path.exists(cloner.repoDir + reponame + "/pom.xml"):
+            #     logging.info(reponame + " is not Maven Project")
+            #     shutil.rmtree(cloner.repoDir + reponame.split('/')[0])
                 #continue; #For Debug
             repo = g.get_repo(reponame)
-            logging.info(reponame + " #commit: " + str(functions.get_commit_count(repo)))
-            out.append(str(functions.get_commit_count(repo)))
-            a = subprocess.run(["sh", "./other/git-loc.sh", reponame], encoding='utf-8', stdout=subprocess.PIPE)
-            match = re.search("SUM:\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)", a.stdout)
-            if match is None:
-                match = re.search(".+\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)", a.stdout)
-            if match:
-                out.append(str(match.group(1)))# files
-                out.append(str(match.group(4)))# code
-            else:
-                out.append(str(0))# files
-                out.append(str(0))# code
+            logging.info(reponame + " #commit: " + functions.get_commit_count(repo))
+            out.append(functions.get_commit_count(repo))
+            logging.info(reponame + " #loc: " + functions.get_loc(reponame))
+            out.append(functions.get_loc(reponame))
+            logging.info(reponame + " #star: " + functions.get_star_count(repo))
+            
             break
     except UnknownObjectException:
         logging.error(reponame + " not found:404")
