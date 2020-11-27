cf6fe022f26c9d721214ffcc6e95be21a7bad4b1
Shimizu Kazuki <shimizu@poslnoMacBook-Pro-2.local>
2020-11-27 13:44:27 +0900

    Get #LOC #4 Fixed

--- other/git-loc.sh
+++ other/git-loc.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-git clone --depth 1 http://github.com/$1.git temp-linecount-repo &&
-  cloc temp-linecount-repo --exclude-lang="Markdown,AsciiDoc,XML,AutoHotkey,JSON,CSON,AMPLE,Ant,ANTLR Grammar,Arduino Sketch,Bazel,builder,CMake,diff,DOORS Extension Language,DTD,dtrace,FXML,Gradle,Groovy,HTML,INI,JSON5,JSP,make,Maven,Mojo,MXML,PO File,QML,RAML,Ruby HTML,SVG,TeX,TNSDL,YAML"&&
-  rm -rf temp-linecount-repo
+cloc /Users/shimizu/MavenGetter/repos/input/$1 --exclude-lang="Markdown,AsciiDoc,XML,AutoHotkey,JSON,CSON,AMPLE,Ant,ANTLR Grammar,Arduino Sketch,Bazel,builder,CMake,diff,DOORS Extension Language,DTD,dtrace,FXML,Gradle,Groovy,HTML,INI,JSON5,JSP,make,Maven,Mojo,MXML,PO File,QML,RAML,Ruby HTML,SVG,TeX,TNSDL,YAML"
--- src/main/project.py
+++ src/main/project.py
@@ -22,14 +22,24 @@ def main():
         for reponame in reponames:
             out = []
             cloner.cloneRepo(reponame)
-            #Maven Project should be Deleted
             print(cloner.repoDir + reponame + "/pom.xml")
-            # if not os.path.exists(cloner.repoDir + reponame + "pom.xml"):
-            #     logging.info(reponame + " is not Maven Project")
-            #     shutil.rmtree(cloner.repoDir + reponame.split('/')[0])
+            if not os.path.exists(cloner.repoDir + reponame + "/pom.xml"):
+                logging.info(reponame + " is not Maven Project")
+                shutil.rmtree(cloner.repoDir + reponame.split('/')[0])
+                #continue; #For Debug
             repo = g.get_repo(reponame)
             logging.info(reponame + " #commit: " + str(functions.get_commit_count(repo)))
             out.append(str(functions.get_commit_count(repo)))
+            a = subprocess.run(["sh", "./other/git-loc.sh", reponame], encoding='utf-8', stdout=subprocess.PIPE)
+            match = re.search("SUM:\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)", a.stdout)
+            if match is None:
+                match = re.search(".+\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)", a.stdout)
+            if match:
+                out.append(str(match.group(1)))# files
+                out.append(str(match.group(4)))# code
+            else:
+                out.append(str(0))# files
+                out.append(str(0))# code
             break
     except UnknownObjectException:
         logging.error(reponame + " not found:404")
